a35171f3264e543cbf54dba1ec3be16e1a735313
marrkonda <76025903+marrkonda@users.noreply.github.com>
2022-04-02 15:17:22 +0700

    Update sample.sh

--- sample.sh
+++ sample.sh
@@ -12,10 +12,7 @@ echo "COPY ./templates /home/myapp/templates/" >> tempdir/Dockerfile
 echo "COPY sample-app.py /home/myapp/" >> tempdir/Dockerfile
 echo "EXPOSE 5050" >> tempdir/Dockerfile
 echo "CMD python3 /home/myapp/sample-app.py" >> tempdir/Dockerfile
-echo "FROM jenkins/jenkins:lts"
-echo "USER root"
-echo "RUN apt-get update"
-echo "RUN curl -sSL https://get.docker.com/ | sh"
+
 cd tempdir
 docker build -t sampleapp .
 docker run -t -d -p 5050:5050 --name samplerunning sampleapp
